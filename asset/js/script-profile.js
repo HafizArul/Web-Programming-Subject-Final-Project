5bb61d4c12311b43ae7d9c384691626e53e9c596
Hafiz Arul <hafizarul1604@outlook.co.id>
2025-07-16 15:30:21 +0700

    Menambah responsive pada profile.html dan fitur interaksinya di script-profile.js

--- asset/js/script-profile.js
+++ asset/js/script-profile.js
@@ -1,3 +1,4 @@
+// FORM SCRIPTS
 // Button Elements
 const btnEditProfile = document.getElementById("editProfile");
 const btnResetForm = document.getElementById("resetForm");
@@ -5,6 +6,9 @@ const btnSave = document.getElementById("saveProfile");
 
 // User Detail
 const userDetails = document.querySelectorAll("#user-detail-biodata.col p");
+
+// Get User Data From localStorage Object
+const user = JSON.parse(localStorage.loggedInUser);
 
 // Input Elements
 const inputUserData = document.getElementsByClassName("edit-user-data-form");
@@ -28,9 +32,13 @@ btnEditProfile.addEventListener("click", function () {
     btnResetForm.removeAttribute("disabled");
     btnResetForm.classList.toggle("btn-secondary");
     btnResetForm.classList.toggle("btn-danger");
-    btnSave.classList.toggle("btn-secondary");
-    btnSave.classList.toggle("btn-success");
-    btnSave.removeAttribute("disabled");
+    for (let i = 0; i < inputUserData.length; i++) {
+        inputUserData[i].addEventListener("input", function () {
+            btnSave.classList.add("btn-success");
+            btnSave.classList.remove("btn-secondary");
+            btnSave.removeAttribute("disabled");
+        });
+    }
 })
 
 // Reset Form
@@ -42,3 +50,48 @@ btnResetForm.addEventListener("click", function () {
         }
     }
 })
+
+// Save Form Changes
+let newUserName, newUserStatus;
+for (let i = 0; i < inputUserData.length; i++) {
+    inputUserData[i].addEventListener("input", function () {
+        newUserName = inputUserData[0].value;
+        newUserStatus = inputUserData[1].value;
+    });
+}
+
+btnSave.addEventListener("click", function () {
+    // Save Username
+    user.name = newUserName;
+    // Save User Status
+    user.status = newUserStatus;
+
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    location.reload();
+})
+
+// USER AUTHENTICATION
+const desktopGreeting = document.getElementById("desktopGreeting");
+const mobileGreeting = document.getElementById("mobileGreeting");
+
+if (user) {
+    const firstName = user.name.split(" ")[0];
+    desktopGreeting.innerHTML = `Halo, ${firstName}`;
+    mobileGreeting.innerHTML = `Halo, ${firstName}`;
+
+    // Mobile Login or Logout Button
+    document.getElementById("mobileAuthBtn").innerHTML = "Logout";
+    document.getElementById("mobileAuthBtn").addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.removeItem("loggedInUser");
+        location.reload();
+    })
+
+    // Desktop Login or Logout Button
+    document.getElementById("desktopAuthBtn").innerHTML = "Logout";
+    document.getElementById("desktopAuthBtn").addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.removeItem("loggedInUser");
+        location.reload();
+    })
+}
